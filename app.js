6d0fd0142f7206e0ad3ef583e1685d1261acb1e2
lisa-cybersecthreat <87460560+lisa-cybersecthreat@users.noreply.github.com>
2021-08-03 11:05:30 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -8,10 +8,27 @@ const dotenv = require("dotenv")
 
 const app= express();
 dotenv.config()
-app.use(cors({
+// app.use(cors({
 //     origin: true,
 //     credentials: true
-}))
+// }))
+app.use( (req, res, next) =>{
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();// Pass to next layer of middleware
+});
+
 app.use(express.json({extended: false}))
 
 app.get("/", (req, res) => { res.send(" heollo world ")})
